c6cb23aa87c4a6a68b2bd5bb9f4d8d1e5f3b9c5e
clumsy261 <rucareanueric@gmail.com>
2026-04-16 12:45:39 +0300

    almost finished the arduino code

--- software/src/src/flower.ino
+++ software/src/src/flower.ino
@@ -3,6 +3,7 @@
 
 const String api="hashed password";//hashed password
 const String user="user";//username
+#define HIGH_HUMID 2000
 
 #include "WiFi.h"
 #include <string>
@@ -12,14 +13,19 @@ void setup() {
   Serial.println("Initializing WiFi...");
   WiFi.mode(WIFI_STA);
   Serial.println("Setup done!");
+  configTime(0,0, "pool.ntp.org");
   pinMode(34,INPUT);//first humidity sensor -- analogRead
   pinMode(35,INPUT);//second humidity sensor -- analogRead
   pinMode(36,INPUT);//water tank sensor -- digitalRead
-  pinMode(39,INPUT);//height sensor
+  pinMode(39,INPUT);//height sensor -- digitalRead
   pinMode(17,OUTPUT);//height motor output
   pinMode(14,OUTPUT);//growing light output
   pinMode(12,OUTPUT);//water pump output
+  pinMode(23,OUTPUT);
+  pinMode(22,INPUT);
 }
+#define us_trig 23  
+#define us_echo 22
 #define soil_1 34
 #define soil_2 35
 #define water 36
@@ -27,17 +33,24 @@ void setup() {
 #define motor 17
 #define led 14
 #define pump 12
-int water_ratio;
-int led_ratio;
+int water_ratio=50;
+int led_ratio=12;
 
 int read_height()
 {
-	///update the schematic to add an ir distance sensor and implement it here
+  digitalWrite(us_trig, LOW);
+  delayMicroseconds(2);
+  digitalWrite(us_trig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(us_trig, LOW);
+  long duration = pulseIn(us_echo, HIGH);
+  float distance = (duration * 0.0343)/2;///float value in cm
+  return (int)distance;///return value in cm
 }
-int read_humidity()
+int read_humidity() //return value from 0 to 4095
   {
     int a=analogRead(soil_1),b=analogRead(soil_2);
-    return a+b/2;
+    return (a+b)/2;
   }
 bool read_tank()
   {
@@ -68,7 +81,15 @@ settings fetch_http(settings s) //send height and tank info in a settings struct
 }
 void lift_plant()
 {
-	///while the touch sensor on the ceiling is active use the motor to lift it up and update height
+	if(digitalRead(touch)) return;
+  bool more = 1;
+  digitalWrite(motor, HIGH);
+  while(more)
+  {
+    delay(500);
+    more = digitalRead(touch);///while touching is active return 1
+  }
+  digitalWrite(motor,LOW);
 }
 
 void get_plant_data()
@@ -76,14 +97,32 @@ void get_plant_data()
 	s.second = read_tank();
 	s.first = read_height();
 }
-
+bool inrange(int a,int water_ratio)///range for the humidity (from 0 to 4095)
+{
+  int p=HIGH_HUMID*water_ratio/100;
+  if(a<=p) return 1;
+  return 0;
+}
+void water_plant()
+{
+  digitalWrite(pump,HIGH);
+  delay(3000);
+  digitalWrite(pump,LOW);
+}
+int get_hour()
+{
+  time_t now;
+  struct tm timeinfo;
+  time(&now);
+  localtime_r(&now,&timeinfo);
+  return timeinfo.tm_hour;
+}
 void loop() {
   //the loop should do this:
-	//TODO get the humidity and water the plant if necesary - also use water_ratio
-
-	///TODO calculate the growing light time the plants get and turn them on/off
-
-	//TODO find the tank status and height of the plant
+	//DONE get the humidity and water the plant if necesary - also use water_ratio
+  int humid=read_humidity();
+  if(inrange(humid,water_ratio)) water_plant();
+	//DONE find the tank status and height of the plant
 	get_plant_data(); //stored in universal struct s
 	///TODO connect to wifi
 	Serial.println("Scanning..."); 
@@ -108,11 +147,16 @@ void loop() {
     }
   }
   Serial.println("");
-	///TODO send data to web portal and retrieve settings
+  ///DONE calculate the growing light time the plants get and turn them on/off
+  int hour = get_hour()+1;
+  if(hour<led_ratio) digitalWrite(led,HIGH);
+  else
+  digitalWrite(led,LOW);
+	///DONE send data to web portal and retrieve settings
 	s = fetch_http(s);
-	///TODO apply settings -done
+	///DONE apply 
 	water_ratio=s.first;
-	led_ratio = s.second;
+	led_ratio = s.second%24;
 
 
   // Wait a bit before scanning again
